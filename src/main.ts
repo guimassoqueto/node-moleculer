ad1b183f28db569d3f8dd2b6ed4b16d4b902a4a4
guimassoqueto <gui.massoqueto@gmail.com>
2023-11-07 06:37:40 -0300

    chore: basic app

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
-import { ServiceBroker } from "mo"
+import { ServiceBroker, Context } from "moleculer"
+
+// create a broker
+const broker = new ServiceBroker()
+
+// define a service
+broker.createService({
+    name: "math",
+    actions: {
+        add(ctx: Context<{a: number, b: number}>): number {
+            return ctx.params.a + ctx.params.b
+        }
+    }
+})
+
+broker.createService({
+    name: "strings",
+    actions: {
+        concat(ctx: Context<{a: string, b: string}>): string {
+            return ctx.params.a + " " + ctx.params.b
+        }
+    }
+})
+
+// start the broker
+await broker.start()
+const responseAdd = await broker.call("math.add", {a: 5, b: 10})
+console.log("math.add:", responseAdd)
+
+const responseConcat = await broker.call("strings.concat", {a: "guilherme", b: "massoqueto"})
+console.log("strings.concat:", responseConcat)
